fdc32847814fabfe32fb55f047ef45110bed6385
xingchen <1144290241@qq.com>
2014-08-10 10:29:24 +0800

    修改： 增加对系统关机或注销的响应，使得它不会在系统关机中因没有保存而丢失数据（当然，对于异常关机、系统奔溃还是没有办法）

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QCoreApplication>
 
 #include "traycontroller.h"
 
@@ -8,6 +9,11 @@ int main(int argc, char *argv[])
 
     TrayController * t = new TrayController();
     t->show();
+
+    // 捕获 commitDataRequest(QSessionManager&) 消息
+    // 使得程序得知系统将要关机或者注销
+    QObject::connect (&a,SIGNAL(commitDataRequest(QSessionManager&)),
+                      t,SLOT(quitSlot()));
 
     return a.exec();
 }
